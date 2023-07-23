4fe66ebcf3b0e7b310a23803c8f99a5a6db3fa09
Maarten Marsman <m.marsman@uva.nl>
2023-07-23 21:47:24 +0200

    This commit contains software to simulate data from the ordinal cross-lagged panel mrf.

---
+++ R/sample_panel_MRF.R
@@ -0,0 +1,254 @@
+#' Sample states of an ordinal cross-lagged panel MRF
+#'
+#' This function samples states from the ordinal cross-lagged panel MRF using a
+#' Gibbs sampler. The Gibbs sampler is initiated with random values from the
+#' response options, after which it proceeds by simulating states for each node
+#' from a logistic model using the other node states as predictor variables.
+#'
+#' @param no_states The number of states of the ordinal cross-lagged panel MRF
+#' to be generated. Must be a positive integer.
+#'
+#' @param no_nodes The number of nodes in the ordinal cross-lagged panel MRF.
+#' Must be an integer greater than or equal to two.
+#'
+#' @param no_timepoints The number of timepoints in the ordinal cross-lagged
+#' panel MRF. Must be a positive integer. The sampler also generates a vector of
+#' responses for time \code{t = 0}, which are conditioned on at time \code{t = 1}.
+#'
+#' @param no_categories Either a positive integer or a vector of positive
+#' integers of length \code{no_nodes}. The number of response categories on top
+#' of the base category: \code{no_categories = 1} generates binary states.
+#'
+#' @param cross_sectional_interactions A symmetric \code{no_nodes} by
+#' \code{no_nodes} matrix of pairwise cross-sectional interactions. Only its
+#' off-diagonal elements are used.
+#'
+#' @param cross_lagged_interactions A \code{no_nodes} by \code{no_nodes} matrix
+#' of pairwise cross-lagged interactions.
+#'
+#' @param thresholds A \code{no_nodes * no_timepoints} by
+#' \code{max(no_categories)} matrix of category thresholds. The first
+#' \code{no_nodes} rows correspond to the thresholds at time \code{t = 1}, the
+#' second \code{no_nodes} rows correspond to the thresholds at time \code{t = 2},
+#' etc. If \code{no_categories} is a vector, only the first
+#' \code{no_categories[r]} elements are used in row \code{r}.
+#'
+#' @param null_interactions A \code{no_nodes} by \code{no_nodes} matrix
+#' of pairwise interactions used to generate observations at time \code{t = 0}.
+#'
+#' @param null_thresholds A \code{no_nodes} by \code{max(no_categories)} matrix
+#' of category thresholds used to generate observations at time \code{t = 0}. If
+#' \code{no_categories} is a vector, only the first \code{no_categories[r]}
+#' elements are used in row \code{r}.
+#'
+#' @param iter The number of iterations used by the Gibbs sampler.
+#' The function provides the last state of the Gibbs sampler as output. By
+#' default set to \code{1e3}.
+#'
+#' @return A \code{no_states} by \code{no_nodes * (no_timepoints + 1)} matrix of
+#' simulated states of the ordinal cross-lagged panel MRF. The first
+#' \code{no_nodes} columns correspond to the observations at time \code{t = 0},
+#' the second \code{no_nodes} columns to the observations at time \code{t = 1},
+#' etc.
+#'
+#' @examples
+#' # Generate responses from a network of five ordinal variables on five timepoints.
+#' no_states = 1000
+#' no_nodes = 5
+#' no_timepoints = 5
+#' no_categories = 2
+#'
+#' u = runif(no_nodes, min = -.5, max = 1)
+#' cross_sectional_interactions = .1 * u %*% t(u)
+#' u = runif(no_nodes, min = -.5, max = 1)
+#' cross_lagged_interactions = u %*% t(u)
+#' thresholds = matrix(-abs(rnorm(no_timepoints * no_nodes * no_categories, 2)),
+#'   nrow = no_timepoints * no_nodes, ncol = no_categories)
+#'
+#' null_interactions = 2 * cross_sectional_interactions
+#' null_thresholds = thresholds[1:no_nodes, ]
+#' x = panelmrfSampler (no_states = no_states,
+#'   no_nodes = no_nodes,
+#'   no_timepoints = no_timepoints,
+#'   no_categories,
+#'   cross_sectional_interactions,
+#'   cross_lagged_interactions,
+#'   thresholds,
+#'   null_interactions,
+#'   null_thresholds,
+#'   iter = 1e4)
+
+#' @export
+panelmrfSampler = function(no_states,
+                           no_nodes,
+                           no_timepoints,
+                           no_categories,
+                           cross_sectional_interactions,
+                           cross_lagged_interactions,
+                           thresholds,
+                           null_interactions,
+                           null_thresholds,
+                           iter = 1e3) {
+  #check no_states, no_nodes, iter
+  if(no_states <= 0 ||
+     abs(no_states - round(no_states)) > .Machine$double.eps^.5)
+    stop("``no_states'' needs be a positive integer.")
+  if(no_nodes <= 0 ||
+     abs(no_nodes - round(no_nodes)) > .Machine$double.eps^.5)
+    stop("``no_nodes'' needs be a positive integer.")
+  if(iter <= 0 ||
+     abs(iter - round(iter)) > .Machine$double.eps^.5)
+    stop("``iter'' must be a positive integer.")
+  if(no_timepoints < 1)
+    stop("``no_timepoints'' must be a positive integer.")
+
+  #check no_categories
+  if(length(no_categories) == 1) {
+    if(no_categories <= 0 ||
+       abs(no_categories - round(no_categories)) > .Machine$double.eps^.5)
+      stop("``no_categories'' needs be a (vector of) positive integer(s).")
+    no_categories = rep(no_categories, no_nodes * no_timepoints)
+  } else {
+    for(node in 1:no_nodes) {
+      if(no_categories[node] <= 0 ||
+         abs(no_categories[node] - round(no_categories[node])) >
+         .Machine$double.eps^.5)
+        stop(paste("For node", node, "``no_categories'' was not a positive
+                   integer."))
+    }
+  }
+
+  #check interactions
+  if(!isSymmetric(cross_sectional_interactions))
+    stop("The matrix ``cross_sectional_interactions'' needs to be symmetric.")
+  diag(cross_sectional_interactions) = 0
+
+  if(!isSymmetric(null_interactions))
+    stop("The matrix ``null_interactions'' needs to be symmetric.")
+  diag(null_interactions) = 0
+
+  if(nrow(cross_sectional_interactions) != no_nodes)
+    stop("The matrix ``cross_sectional_interactions'' needs to have ``no_nodes'' rows and columns.")
+
+  if(nrow(cross_lagged_interactions) != no_nodes ||
+     ncol(cross_lagged_interactions) != no_nodes)
+    stop("The matrix ``cross_lagged_interactions'' needs to have ``no_nodes'' rows and columns.")
+
+  if(nrow(null_interactions) != no_nodes)
+    stop("The matrix ``null_interactions'' needs to have ``no_nodes'' rows and columns.")
+
+  #check thresholds
+  if(!inherits(thresholds, what = "matrix")) {
+    if(max(no_categories) == 1) {
+      if(length(thresholds) == no_nodes * no_timepoints) {
+        thresholds = matrix(thresholds, ncol = 1)
+      } else {
+        stop(paste0("The matrix ``thresholds'' has ",
+                    length(thresholds),
+                    " elements, but requires",
+                    no_nodes * no_timepoints,
+                    "."))
+      }
+    } else {
+      stop("Argument ``thresholds'' needs to be a matrix.")
+    }
+  }
+
+  if(!inherits(null_thresholds, what = "matrix")) {
+    if(max(no_categories) == 1) {
+      if(length(null_thresholds) == no_nodes) {
+        null_thresholds = matrix(null_thresholds, ncol = 1)
+      } else {
+        stop(paste0("The matrix ``null_thresholds'' has ",
+                    length(null_thresholds),
+                    " elements, but requires",
+                    no_nodes,
+                    "."))
+      }
+    } else {
+      stop("Argument ``null_thresholds'' needs to be a matrix.")
+    }
+  }
+
+  if(nrow(thresholds) != no_nodes * no_timepoints)
+    stop("The matrix ``thresholds'' needs to have ``no_nodes * no_timepoints'' rows.")
+
+  if(nrow(null_thresholds) != no_nodes)
+    stop("The matrix ``null_thresholds'' needs to have ``no_nodes'' rows.")
+
+  for(t in 1:no_timepoints) {
+    start = (t - 1) * no_nodes + 1
+    stop = t * no_nodes
+    for(node in start:stop) {
+      if(any(is.na(thresholds[node, 1:no_categories[node-start+1]]))) {
+        stop(paste0("The matrix ``thresholds'' contains NA(s) for node ",
+                    node-start+1,
+                    "at time",
+                    t,
+                    " in categorie(s)",
+                    which(is.na(thresholds[node, 1:no_categories[node-start+1]])),
+                    ", where a numeric value is needed."))
+      }
+      if(ncol(thresholds) > no_categories[node-start+1]) {
+        if(any(!is.na(thresholds[node, (no_categories[node-start+1]+1):ncol(thresholds)]))){
+          warning(paste0("The matrix ``thresholds'' contains numeric values for node ",
+                         node,
+                         "at time",
+                         t,
+                         " for categories(s) (i.e., columns) exceding the maximum of ",
+                         no_categories[node-start+1],
+                         ". These values will be ignored."))
+        }
+      }
+    }
+
+    for(node in start:stop) {
+      for(category in 1:no_categories[node-start+1]) {
+        if(!is.finite(thresholds[node, category]))
+          stop(paste("The threshold parameter for node", node, "and category",
+                     category, "at t =", t, "is NA or not finite."))
+      }
+    }
+  }
+
+  for(node in 1:no_nodes) {
+    if(any(is.na(null_thresholds[node, 1:no_categories[node]]))) {
+      stop(paste0("The matrix ``null_thresholds'' contains NA(s) for node ",
+                  node,
+                  " in categorie(s)",
+                  which(is.na(null_thresholds[node, 1:no_categories[node]])),
+                  ", where a numeric value is needed."))
+    }
+    if(ncol(null_thresholds) > no_categories[node]) {
+      if(any(!is.na(null_thresholds[node, (no_categories[node]+1):ncol(null_thresholds)]))){
+        warning(paste0("The matrix ``null_thresholds'' contains numeric values for node ",
+                       node,
+                       " for categories(s) (i.e., columns) exceding the maximum of ",
+                       no_categories[node],
+                       ". These values will be ignored."))
+      }
+    }
+  }
+
+  for(node in 1:no_nodes) {
+    for(category in 1:no_categories[node]) {
+      if(!is.finite(thresholds[node, category]))
+        stop(paste("The threshold parameter for node", node, "and category",
+                   category, " at t = 0 is NA or not finite."))
+    }
+  }
+
+
+  x = sample_o_clp_mrf_gibbs(no_states = no_states,
+                             no_nodes = no_nodes,
+                             no_timepoints = no_timepoints,
+                             no_categories = no_categories,
+                             cross_sectional_interactions = cross_sectional_interactions,
+                             cross_lagged_interactions = cross_lagged_interactions,
+                             thresholds = thresholds,
+                             null_interactions = null_interactions,
+                             null_thresholds = null_thresholds,
+                             iter)
+
+  return(x)
+}
--- src/data_simulation.cpp
+++ src/data_simulation.cpp
@@ -8,7 +8,7 @@ IntegerMatrix sample_omrf_gibbs(int no_states,
                                 NumericMatrix interactions,
                                 NumericMatrix thresholds,
                                 int iter) {
-  
+
   IntegerMatrix observations(no_states, no_nodes);
   int max_no_categories = 0;
   for(int node = 0; node < no_nodes; node++) {
@@ -22,7 +22,7 @@ IntegerMatrix sample_omrf_gibbs(int no_states,
   double cumsum = 0.0;
   double u = 0.0;
   int score = 0;
-  
+
   //Random (uniform) starting values -------------------------------------------
   for(int node = 0; node < no_nodes; node++) {
     for(int person =  0; person < no_states; person++) {
@@ -32,9 +32,9 @@ IntegerMatrix sample_omrf_gibbs(int no_states,
         cumsum += 1;
         probabilities[category + 1] = cumsum;
       }
-      
+
       u = cumsum * R::unif_rand();
-      
+
       score = 0;
       while (u > probabilities[score]) {
         score++;
@@ -42,7 +42,7 @@ IntegerMatrix sample_omrf_gibbs(int no_states,
       observations(person, node) = score;
     }
   }
-  
+
   //The Gibbs sampler ----------------------------------------------------------
   for(int iteration = 0; iteration < iter; iteration++) {
     for(int node = 0; node < no_nodes; node++) {
@@ -52,18 +52,18 @@ IntegerMatrix sample_omrf_gibbs(int no_states,
           rest_score += observations(person, vertex) *
             interactions(vertex, node);
         }
-        
+
         cumsum = 1.0;
         probabilities[0] = 1.0;
         for(int category = 0; category < no_categories[node]; category++) {
-          exponent = thresholds(node, category); 
+          exponent = thresholds(node, category);
           exponent += (category + 1) * rest_score;
           cumsum += std::exp(exponent);
           probabilities[category + 1] = cumsum;
         }
 
         u = cumsum * R::unif_rand();
-        
+
         score = 0;
         while (u > probabilities[score]) {
           score++;
@@ -73,6 +73,132 @@ IntegerMatrix sample_omrf_gibbs(int no_states,
     }
     Rcpp::checkUserInterrupt();
   }
-  
+
+  return observations;
+}
+
+// [[Rcpp::export]]
+IntegerMatrix sample_o_clp_mrf_gibbs(int no_states,
+                                      int no_nodes,
+                                      int no_timepoints,
+                                      IntegerVector no_categories,
+                                      NumericMatrix cross_sectional_interactions,
+                                      NumericMatrix cross_lagged_interactions,
+                                      NumericMatrix thresholds,
+                                      NumericMatrix null_interactions,
+                                      NumericMatrix null_thresholds,
+                                      int iter) {
+
+  IntegerVector start(no_timepoints);
+  IntegerVector stop(no_timepoints);
+  for(int t = 0; t <= no_timepoints; t++) {
+    start[t] = t * no_nodes;
+    stop[t] = (t + 1) * no_nodes - 1;
+  }
+
+  IntegerMatrix observations(no_states, no_nodes * (no_timepoints + 1));
+  int max_no_categories = 0;
+  for(int node = 0; node < no_nodes; node++) {
+    if(no_categories[node] > max_no_categories) {
+      max_no_categories = no_categories[node];
+    }
+  }
+  NumericVector probabilities(max_no_categories + 1);
+  double exponent = 0.0;
+  double rest_score = 0.0;
+  double cumsum = 0.0;
+  double u = 0.0;
+  int score = 0;
+
+  //Random (uniform) starting values -------------------------------------------
+  for(int t = 0; t < no_timepoints; t++) {
+    for(int node = 0; node < no_nodes; node++) {
+      for(int person =  0; person < no_states; person++) {
+        cumsum = 1.0;
+        probabilities[0] = 1.0;
+        for(int category = 0; category < no_categories[node]; category++) {
+          cumsum += 1;
+          probabilities[category + 1] = cumsum;
+        }
+
+        u = cumsum * R::unif_rand();
+
+        score = 0;
+        while (u > probabilities[score]) {
+          score++;
+        }
+        observations(person, start[t] + node) = score;
+      }
+    }
+  }
+
+  //The Gibbs sampler ----------------------------------------------------------
+  for(int iteration = 0; iteration < iter; iteration++) {
+    //We start with sampling states at t = 0 -----------------------------------
+    for(int node = 0; node < no_nodes; node++) {
+      for(int person =  0; person < no_states; person++) {
+        rest_score = 0.0;
+        for(int vertex = 0; vertex < no_nodes; vertex++) {
+          rest_score += observations(person, vertex) *
+            null_interactions(vertex, node);
+        }
+
+        cumsum = 1.0;
+        probabilities[0] = 1.0;
+        for(int category = 0; category < no_categories[node]; category++) {
+          exponent = null_thresholds(node, category);
+          exponent += (category + 1) * rest_score;
+          cumsum += std::exp(exponent);
+          probabilities[category + 1] = cumsum;
+        }
+
+        u = cumsum * R::unif_rand();
+
+        score = 0;
+        while (u > probabilities[score]) {
+          score++;
+        }
+        observations(person, node) = score;
+      }
+    }
+    Rcpp::checkUserInterrupt();
+  }
+
+  //Now we sample the rest of the time points in turn --------------------------
+  for(int t = 1; t <= no_timepoints; t++) {
+    for(int node = 0; node < no_nodes; node++) {
+      for(int person =  0; person < no_states; person++) {
+        rest_score = 0.0;
+        for(int node2 = 0; node2 < no_nodes; node2++) {
+          // Cross-sectional elements --------------------------------------------
+          rest_score += cross_sectional_interactions(node, node2) *
+            observations(person, start[t] + node2);
+
+          // Cross-lagged elements --------------------------------------------
+          rest_score += cross_lagged_interactions(node, node2) *
+            observations(person, start[t-1] + node2);
+        }
+
+        cumsum = 1.0;
+        probabilities[0] = 1.0;
+        for(int category = 0; category < no_categories[node]; category++) {
+          exponent = thresholds(node + start[t-1], category);
+          exponent += (category + 1) * rest_score;
+          cumsum += std::exp(exponent);
+          probabilities[category + 1] = cumsum;
+        }
+
+        u = cumsum * R::unif_rand();
+
+        score = 0;
+        while (u > probabilities[score]) {
+          score++;
+        }
+        observations(person, start[t] + node) = score;
+      }
+    }
+    Rcpp::checkUserInterrupt();
+  }
+
   return observations;
 }
