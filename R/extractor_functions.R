f06ee2eee0fdc8b6b925fb5a5e1f3eae080234ab
Don van den Bergh <donvdbergh@hotmail.com>
2024-09-20 12:47:10 +0200

    example S3 class

--- R/extractor_functions.R
+++ R/extractor_functions.R
@@ -8,10 +8,11 @@
 #' @keywords internal
 #' @export
 extract_arguments <- function(bgms_object) {
-  if(!inherits(bgms_object, what = "bgms"))
-    stop(paste0("Expected an object with class bgms and not one with class ",
-      class(bgms_object)))
+  UseMethod("extract_arguments")
+}
 
+#' @export
+extract_arguments.bgms <- function(bgms_object) {
   if(is.null(bgms_object$arguments)) {
     stop(paste0("Extractor functions have been defined for bgms versions 0.1.3 and up but not \n",
                 "for older versions. The current fit object predates version 0.1.3."))
@@ -19,6 +20,7 @@ extract_arguments <- function(bgms_object) {
     return(bgms_object$arguments)
   }
 }
+
 
 #' @rdname extractor_functions
 #' @export
