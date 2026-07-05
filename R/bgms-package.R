fc51dd6c03b73a6c473825f0300f24879afb70b7
Maarten Marsman <m.marsman@uva.nl>
2026-07-05 14:17:44 +0200

    Import stats functions used by the Z-ratio and ancestral-init helpers
    R CMD check noted approxfun/dnorm/integrate/rbeta/rexp/rgamma/rnorm/rpois/
    runif as undefined globals in zratio_tables.R and the ancestral-init
    functions. Declare them via @importFrom stats in the package doc.

--- R/bgms-package.R
+++ R/bgms-package.R
@@ -64,6 +64,8 @@
 #' @docType package
 #' @keywords internal
 #' @useDynLib bgms, .registration=TRUE
+#' @importFrom stats approxfun dnorm integrate
+#' @importFrom stats rbeta rexp rgamma rnorm rpois runif
 #' @references
 #' \insertAllCited{}
 "_PACKAGE"
